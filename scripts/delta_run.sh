8bf3f93b1e74f8897d1270323c2ce1cec2e966ed
wang-zhanyu <zhanyu_w16@163.com>
2023-08-29 20:27:20 +0800

    first commit

---
+++ scripts/delta_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dataset="mimic_cxr"
+annotation="data/mimic_cxr/my_mimic_anno.json"
+base_dir="./data/mimic_cxr/images"
+
+version="v0_delta"
+savepath="./save/$dataset/$version"
+
+python -u train.py \
+    --dataset ${dataset} \
+    --annotation ${annotation} \
+    --base_dir ${base_dir} \
+    --batch_size 16 \
+    --val_batch_size 16 \
+    --freeze_vm True \
+    --vis_use_lora True \
+    --savedmodel_path ${savepath} \
+    --num_workers 8 \
+    --devices 1 \
+    --max_epochs 3 \
+    2>&1 |tee -a ${savepath}/log.txt
---
+++ train.py
@@ -0,0 +1,47 @@
+import os
+from pprint import pprint
+from configs.config import parser
+from dataset.data_module import DataModule
+from lightning_tools.callbacks import add_callbacks
+from models.R2GenGPT import R2GenGPT
+from lightning.pytorch import seed_everything
+import lightning.pytorch as pl
+
+ 
+def train(args):
+    dm = DataModule(args)
+    callbacks = add_callbacks(args)
+
+    trainer = pl.Trainer(
+        devices=args.devices,
+        num_nodes=args.num_nodes,
+        strategy=args.strategy,
+        accelerator=args.accelerator,
+        precision=args.precision,
+        val_check_interval = args.val_check_interval,
+        limit_val_batches = args.limit_val_batches,
+        max_epochs = args.max_epochs,
+        num_sanity_val_steps = args.num_sanity_val_steps,
+        accumulate_grad_batches=args.accumulate_grad_batches,
+        callbacks=callbacks["callbacks"], 
+        logger=callbacks["loggers"]
+    )
+
+    if args.ckpt_file is not None:
+        model = R2GenGPT.load_from_checkpoint(args.ckpt_file, strict=False)
+    else:
+        model = R2GenGPT(args)
+
+    trainer.fit(model, datamodule=dm)
+
+
+def main():
+    args = parser.parse_args()
+    os.makedirs(args.savedmodel_path, exist_ok=True)
+    pprint(vars(args))
+    seed_everything(42, workers=True)
+    train(args)
+
+
+if __name__ == '__main__':
+    main()
